a23fbdaa313b1ba39cebdb8e86118a3c3e2fd3ae
Dawid Kułaga <dawidson.kiano@gmail.com>
2022-03-08 15:28:25 +0100

    Added Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
 	return (
-		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
+		<div className=''>
 			<Head>
 				<title>Airbnb</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-      <h1>Lets build Airbnb</h1>
+      <Header/>
 		</div>
 	);
 };
